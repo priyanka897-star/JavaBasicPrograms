aa628c28cca7f1ff3854bb2b47d34c088f37328d
priyanka897-star <priyankamore8997@gmail.com>
2021-10-01 00:34:20 +0530

    Refactored Content of code

--- basicjavaprograms/EuclidDistance.java
+++ basicjavaprograms/EuclidDistance.java
@@ -6,11 +6,11 @@ public class EuclidDistance {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter X Point");
-		int x = scanner.nextInt();
+		final int x = scanner.nextInt();
 		System.out.println("Enter Y Point");
-		int y = scanner.nextInt();
+		final int y = scanner.nextInt();
+		scanner.close();
 		double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 		System.out.println("The Euclidean distance is :" + distance);
 	}
-
 }
