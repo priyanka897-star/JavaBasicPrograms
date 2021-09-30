1cf166b4e4c16a5dd3e36bb0fb895d0da402aa61
priyanka897-star <priyankamore8997@gmail.com>
2021-09-30 16:18:04 +0530

    Refactored:Content of the Program,Indentation Correction.

--- basicjavaprograms/FlipCoin.java
+++ basicjavaprograms/FlipCoin.java
@@ -3,32 +3,34 @@ package basicjavaprograms;
 import java.util.Scanner;
 
 public class FlipCoin {
+
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Number of  how  many Times you want to Flip coin:");
+		final int noOfFlips = scanner.nextInt();//Constant
+		scanner.close();
 		int heads = 0;
 		int tails = 1;
-		int counter = 1;
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter Number of  how  many Times you want to Flip coin :");
-		int noOfFlips = scanner.nextInt();
-		if (noOfFlips >= 0) // if noOfFlips are positive then the Program will execute
+        if (noOfFlips >= 0) // if noOfFlips are positive then the Program will execute
 		{
-			while (counter != noOfFlips) {
-				counter++;
-				int random = (int) Math.floor(Math.random() * 2);
-				if (random == 0)
+        	int random = (int) Math.floor(Math.random() * 2);
+			for (int i = 0; i < noOfFlips; i++) {
+				if (random > 0.5) {
 					tails++;
-				else
+				} 
+				else {
 					heads++;
+				}
 			}
 			System.out.println("Number of Times Heads comes :" + heads);
 			System.out.println("Number of Times Tails comes :" + tails);
-			double tailsPercentage = (double) tails / noOfFlips * 100;
-			double headsPercentage = (double) heads / noOfFlips * 100;
+			int tailsPercentage =(100 * tails) / noOfFlips;
+			int headsPercentage =(100 * heads) / noOfFlips;;
 			System.out.println("Heads Percentage is :" + headsPercentage);
 			System.out.println("Tails Percentage is :" + tailsPercentage);
 		}
-
-		else {
+        else{
 			System.out.println("Enter Positive Number");
 		}
 	}
