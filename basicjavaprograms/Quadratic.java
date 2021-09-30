e7b2d82ec8dc35517cf28131cabc33e3b8e68222
priyanka897-star <priyankamore8997@gmail.com>
2021-10-01 00:36:46 +0530

    Refactored Content of code

--- basicjavaprograms/Quadratic.java
+++ basicjavaprograms/Quadratic.java
@@ -6,9 +6,10 @@ public class Quadratic {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter a,b,c Values  ");// a*x*x + b*x + c.
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		int c = scanner.nextInt();
+		 final int a = scanner.nextInt();
+		 final int b = scanner.nextInt();
+		 final int c = scanner.nextInt();
+		 scanner.close();
 		double delta = (b * b) - (4 * a * c);
 		double root1 = (double) (-b + Math.sqrt(delta)) / (2 * a);
 		double root2 = (double) (-b - Math.sqrt(delta)) / (2 * a);
