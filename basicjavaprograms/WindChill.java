c7ccc8c81d05d6f659fdc237642faee51958fa59
priyanka897-star <priyankamore8997@gmail.com>
2021-10-01 00:31:57 +0530

    Refactored Content of code

--- basicjavaprograms/WindChill.java
+++ basicjavaprograms/WindChill.java
@@ -6,15 +6,17 @@ public class WindChill {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("Enter Temperature");
+		System.out.println("Enter Tempereture");
 		double temperature = scanner.nextDouble();
 		System.out.println("Enter Wind Speed");
 		double windSpeed = scanner.nextDouble();
+		scanner.close();
 		double windChill;
 		if (temperature > 50 && (windSpeed < 3 || windSpeed > 120)) {
 			windChill = 35.74 + (0.6215 * temperature) + (((0.4275 * temperature) - 35.75) * Math.pow(windSpeed, 0.16));
 			System.out.println("The WindChill value is :" + windChill);
-		} else {
+		} 
+		else{
 			System.out.println("Enter Valid Tempreture or Wind Speed");
 		}
 	}
