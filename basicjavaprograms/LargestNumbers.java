e925544ddbc7a21cf7c93d791a658af787eb7a81
priyanka897-star <priyankamore8997@gmail.com>
2021-10-01 00:40:32 +0530

    Refactored Content of code

--- basicjavaprograms/LargestNumbers.java
+++ basicjavaprograms/LargestNumbers.java
@@ -1,19 +1,24 @@
 package basicjavaprograms;
+
 import java.util.Scanner;
 
 public class LargestNumbers {
 
 	public static void main(String[] args) {
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter Three Numbers :");
-		int number1 = scanner.nextInt();
-		int number2 = scanner.nextInt();
-		int number3 = scanner.nextInt();
+		final int number1 = scanner.nextInt();
+		final int number2 = scanner.nextInt();
+		final int number3 = scanner.nextInt();
+		scanner.close();
 		if (number1 > number2 && number1 > number3) {
 			System.out.println("Largest Number is :" + number1);
-		} else if (number2 > number1 && number2 > number3) {
+		} 
+		else if (number2 > number1 && number2 > number3) {
 			System.out.println("Largest Number is :" + number2);
-		} else {
+		} 
+		else {
 			System.out.println("Largest Number is :" + number3);
 		}
 	}
